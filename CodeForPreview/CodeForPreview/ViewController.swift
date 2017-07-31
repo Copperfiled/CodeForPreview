7d1533dc0f637e5e59b47a13dea17b5c18942c63
Copperfiled <guan.dassy@gmail.com>
2017-07-31 11:57:24 +0800

    创建新工程

---
+++ CodeForPreview/CodeForPreview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeForPreview
+//
+//  Created by guanxiaobai on 31/07/2017.
+//  Copyright © 2017 guanxiaobai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
